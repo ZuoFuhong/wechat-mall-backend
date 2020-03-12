c1b6caae9478b30d7e05519005bdedc8b007e4aa
zfh <fuhongzuo@gmail.com>
2020-03-12 23:35:09 +0800

    更新build.sh

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ else
 mkdir target
 fi
 # linux
-GOOS=linux GOARCH=amd64 go build -o ./target/web-mall-backend main.go
+GOOS=linux GOARCH=amd64 go build -o ./target/wechat-mall-backend main.go
 # windows
-#GOOS=windows GOARCH=amd64 go build -o ./target/web-mall-backend main.go
+#GOOS=windows GOARCH=amd64 go build -o ./target/wechat-mall-backend main.go
 # macOS
-#go build -o ./target/web-mall-backend main.go
+#go build -o ./target/wechat-mall-backend main.go
 cp -rf conf ./target/
 tar -zcvf ./target/wechat-mall-backend.tar.gz target/
 echo "build Done."
