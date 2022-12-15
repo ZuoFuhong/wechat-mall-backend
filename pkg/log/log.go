0267678016e04bc1e191ba9f849d8e8ab7d083ce
Mars Zuo <marszuo@foxmail.com>
2022-12-15 11:09:20 +0800

    refactor: Enhanced output log call point

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -4,58 +4,51 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"runtime"
+	"os"
 	"wechat-mall-backend/consts"
 )
 
 func Debugf(format string, args ...interface{}) {
-	log.Printf("DEBUG "+format+"\n", args...)
+	_ = log.Output(2, fmt.Sprintf("DEBUG "+format+"\n", args...))
 }
 
 func Infof(format string, args ...interface{}) {
-	log.Printf("INFO "+format+"\n", args...)
+	_ = log.Output(2, fmt.Sprintf("INFO  "+format+"\n", args...))
 }
 
 func Warnf(format string, args ...interface{}) {
-	log.Printf("WARN "+format+"\n", args...)
+	_ = log.Output(2, fmt.Sprintf("WARN  "+format+"\n", args...))
 }
 
 func Errorf(format string, args ...interface{}) {
-	log.Printf("ERROR "+format+"\n", args...)
-	printStack()
+	_ = log.Output(2, fmt.Sprintf("ERROR "+format+"\n", args...))
 }
 
 func Fatalf(format string, args ...interface{}) {
-	log.Fatalf(format, args...)
+	_ = log.Output(2, fmt.Sprintf(format, args...))
+	os.Exit(1)
 }
 
 func DebugContextf(ctx context.Context, format string, args ...interface{}) {
 	traceId := ctx.Value(consts.TraceKey).(string)
 	prefix := fmt.Sprintf("DEBUG %s ", traceId)
-	log.Printf(prefix+format+"\n", args...)
+	_ = log.Output(2, fmt.Sprintf(prefix+format+"\n", args...))
 }
 
 func InfoContextf(ctx context.Context, format string, args ...interface{}) {
 	traceId := ctx.Value(consts.TraceKey).(string)
-	prefix := fmt.Sprintf("INFO %s ", traceId)
-	log.Printf(prefix+format+"\n", args...)
+	prefix := fmt.Sprintf("INFO  %s ", traceId)
+	_ = log.Output(2, fmt.Sprintf(prefix+format+"\n", args...))
 }
 
 func WarnContextf(ctx context.Context, format string, args ...interface{}) {
 	traceId := ctx.Value(consts.TraceKey).(string)
-	prefix := fmt.Sprintf("WARN %s ", traceId)
-	log.Printf(prefix+format+"\n", args...)
+	prefix := fmt.Sprintf("WARN  %s ", traceId)
+	_ = log.Output(2, fmt.Sprintf(prefix+format+"\n", args...))
 }
 
 func ErrorContextf(ctx context.Context, format string, args ...interface{}) {
 	traceId := ctx.Value(consts.TraceKey).(string)
 	prefix := fmt.Sprintf("ERROR %s ", traceId)
-	log.Printf(prefix+format+"\n", args...)
-	printStack()
-}
-
-func printStack() {
-	var buf [4096]byte
-	n := runtime.Stack(buf[:], false)
-	log.Print(string(buf[:n]))
+	_ = log.Output(2, fmt.Sprintf(prefix+format+"\n", args...))
 }
